4585975e946e0a0328777c85f3e4fec91d88a17e
Elias Kupeczki <github@kunoxy.ch>
2023-01-28 11:59:39 +0100

    Everything Works

---
+++ src/main/java/com/example/modul326/DiceLogic.java
@@ -0,0 +1,177 @@
+package com.example.modul326;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class DiceLogic {
+
+    public Integer[] Dice = new Integer[5];
+    Integer DiceAmount = 5;
+    //create a function that returns an array of random numbers between 1 and 6
+    public void DiceGenerator() {
+        for (int i = 0; i < DiceAmount; i++) {
+            Dice[i] = (int) (Math.random() * 6 + 1);
+        }
+    }
+    //1's counter
+    public Integer OneCounter(Integer[] Dice) {
+        Integer OneCounter = 0;
+        for (int i = 0; i < DiceAmount; i++) {
+            if (Dice[i] == 1) {
+                OneCounter++;
+            }
+        }
+        return OneCounter;
+    }
+    //2's counter
+    public Integer TwoCounter(Integer[] Dice) {
+        Integer TwoCounter = 0;
+        for (int i = 0; i < DiceAmount; i++) {
+            if (Dice[i] == 2) {
+                TwoCounter++;
+            }
+        }
+        TwoCounter = TwoCounter * 2;
+        return TwoCounter;
+    }
+    //3's counter
+    public Integer ThreeCounter(Integer[] Dice) {
+        Integer ThreeCounter = 0;
+        for (int i = 0; i < DiceAmount; i++) {
+            if (Dice[i] == 3) {
+                ThreeCounter++;
+            }
+        }
+        ThreeCounter = ThreeCounter * 3;
+        return ThreeCounter;
+    }
+    //4's counter
+    public Integer FourCounter(Integer[] Dice) {
+        Integer FourCounter = 0;
+        for (int i = 0; i < DiceAmount; i++) {
+            if (Dice[i] == 4) {
+                FourCounter++;
+            }
+        }
+        FourCounter = FourCounter * 4;
+        return FourCounter;
+    }
+    //5's counter
+    public Integer FiveCounter(Integer[] Dice) {
+        Integer FiveCounter = 0;
+        for (int i = 0; i < DiceAmount; i++) {
+            if (Dice[i] == 5) {
+                FiveCounter++;
+            }
+        }
+        FiveCounter = FiveCounter * 5;
+        return FiveCounter;
+    }
+    // 6's counter
+    public Integer SixCounter(Integer[] Dice) {
+        Integer SixCounter = 0;
+        for (int i = 0; i < DiceAmount; i++) {
+            if (Dice[i] == 6) {
+                SixCounter++;
+            }
+        }
+        SixCounter = SixCounter * 6;
+        return SixCounter;
+    }
+    //3 of a kind
+    public Integer ThreeOfAKindCounter(Integer[] Dice) {
+        for (Integer die : Dice) {
+            int count = 0;
+            for (Integer integer : Dice) {
+                if (die == integer) {
+                    count++;
+                }
+                if (count == 3) {
+                    return 3*die;
+                }
+            }
+        }
+        return 0;
+    }
+    //4 of a kind
+    public Integer FourOfAKindCounter(Integer[] Dice) {
+        for (Integer die : Dice) {
+            int count = 0;
+            for (Integer integer : Dice) {
+                if (Objects.equals(die, integer)) {
+                    count++;
+                }
+                if (count == 4) {
+                    return 4*die;
+                }
+            }
+        }
+        return 0;
+    }
+    //FullHouse
+    public Integer FullHouseCounter(Integer[] Dice) {
+        boolean FullHouseCounter = false;
+        Integer[] temp = {OneCounter(Dice), TwoCounter(Dice), ThreeCounter(Dice), FourCounter(Dice), FiveCounter(Dice), SixCounter(Dice)};
+        //count how many zeros are in temp
+        int count = 0;
+        for (Integer integer : temp) {
+            if (integer == 0) {
+                count++;
+            }
+        }
+        if (count == 4) {
+            FullHouseCounter = true;
+        }
+        //return the array of temp
+        if (FullHouseCounter) {
+            return 25;
+        } else {
+            return 0;
+        }
+    }
+    //check for small straight
+    public Integer checkForSmallStraight(Integer[] Dice) {
+        for (int i = 0; i < Dice.length - 3; i++) {
+            int count = 0;
+            for (int j = i; j < Dice.length; j++) {
+                if (Dice[j] == Dice[i]+count) {
+                    count++;
+                }
+                if (count == 4) {
+                    return 30;
+                }
+            }
+        }
+        return 0;
+    }
+    //check for large straight
+    public Boolean LargeStraightCounter(Integer[] Dice) {
+        for (int i = 0; i < Dice.length - 4; i++) {
+            int count = 0;
+            for (int j = i; j < Dice.length; j++) {
+                if (Dice[j] == Dice[i]+count) {
+                    count++;
+                }
+                if (count == 5) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    //check for chance
+    public Integer ChanceCounter(Integer[] Dice) {
+        int sum = 0;
+        for (Integer die : Dice) {
+            sum += die;
+        }
+        return sum;
+    }
+    //yahtzee
+    public Integer YahtzeeCounter(Integer[] Dice) {
+        if(Arrays.stream(Dice).allMatch(val -> val == Dice[0])) {
+            return 50;
+        }
+        return 0;
+    }
+}
--- src/main/java/com/example/modul326/DiceRoll.java
+++ src/main/java/com/example/modul326/DiceRoll.java
@@ -5,226 +5,58 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class DiceRoll {
-    public Integer[] Dice = new Integer[6];
-    /*
-Integer DiceAmount = 6;
-Integer[] Dice = {0, 0, 0, 0, 0, 0};
-Label[] labels = {Dice1, Dice2, Dice3, Dice4, Dice5, Dice6};
-
-@FXML
-protected void onHelloButtonClick() {
-
-    for (int i = 0; i < DiceAmount; i++) {
-        //cast double to int
-        Dice[i] = (int) (Math.random() * 6 + 1);
-        labels[i].setText(String.valueOf(Dice[i]));
-    }
-}
- */
-    Integer DiceAmount = 5;
     @FXML
-    private Label Dice1;
+    private Label Dice1, Dice2, Dice3, Dice4, Dice5, One, Two, Three, Four, Five, Six, ThreeOfAKind, FourofaKind, FullHouse, SmallStraight, LargeStraight, Chance, Yahtzee;
     @FXML
-    private Label Dice2;
-    @FXML
-    private Label Dice3;
-    @FXML
-    private Label Dice4;
-    @FXML
-    private Label Dice5;
-    @FXML
-    private Label Dice6;
-    @FXML
-    private Button Roll;
-    @FXML
-    private Label One;
-    @FXML
-    private Label Two;
-    @FXML
-    private Label Three;
-    @FXML
-    private Label Four;
-    @FXML
-    private Label Five;
-    @FXML
-    private Label Six;
-    @FXML
-    private Label ThreeOfAKind;
-    @FXML
-    private Label FourofaKind;
-    @FXML
-    private Label FullHouse;
-    @FXML
-    private Label SmallStraight;
-    @FXML
-    private Label LargeStraight;
-    @FXML
-    private Label Chance;
-    @FXML
-    private Label Yahtzee;
-    @FXML
-    private Label Total;
-
-    //TODO: rewrite the array generator in a new method
-    //create a function that returns an array of random numbers between 1 and 6
-    public Integer[] DiceGenerator() {
-        for (int i = 0; i < DiceAmount; i++) {
-            //cast double to int
-            Dice[i] = (int) (Math.random() * 6 + 1);
+    private Label One1, Two1, Three1, Four1, Five1, Six1, ThreeOfAKind1, FourofaKind1, FullHouse1, SmallStraight1, LargeStraight1, Chance1, Yahtzee1;
+    Integer[] currentArray = new Integer[5];
+    DiceLogic dice = new DiceLogic();
+    int clicked = 0;
+    public void RollDice() {
+        clicked++;
+        DiceLogic dice = new DiceLogic();
+        dice.DiceGenerator();
+        System.arraycopy(dice.Dice, 0, currentArray, 0, dice.Dice.length);
+        TogglePlayer();
         }
-        return Dice;
-    }
-    //create a function with the input from dicegenerator and returns the amount of 1's and added it to an global array
 
-    public Integer OneCounter(Integer[] Dice) {
-        Integer OneCounter = 0;
-        for (int i = 0; i < DiceAmount; i++) {
-            if (Dice[i] == 1) {
-                OneCounter++;
-            }
+    public void TogglePlayer() {
+
+        if (clicked <=3) {
+            SetPlayer1();
         }
-        return OneCounter;
-    }
-
-    //create a function with the input from dicegenerator and returns the amount of 2's
-    public Integer TwoCounter(Integer[] Dice) {
-        Integer TwoCounter = 0;
-        for (int i = 0; i < DiceAmount; i++) {
-            if (Dice[i] == 2) {
-                TwoCounter++;
-            }
+        if (clicked > 3){
+            SetPlayer2();
         }
-        return TwoCounter;
-    }
-
-    //create a function with the input from dicegenerator and returns the amount of 3's
-    public Integer ThreeCounter(Integer[] Dice) {
-        Integer ThreeCounter = 0;
-        for (int i = 0; i < DiceAmount; i++) {
-            if (Dice[i] == 3) {
-                ThreeCounter++;
-            }
+        if (clicked == 6) {
+            clicked = 0;
         }
-        return ThreeCounter;
     }
-
-    //create a function with the input from dicegenerator and returns the amount of 4's
-    public Integer FourCounter(Integer[] Dice) {
-        Integer FourCounter = 0;
-        for (int i = 0; i < DiceAmount; i++) {
-            if (Dice[i] == 4) {
-                FourCounter++;
-            }
+    public void setText(){
+        Dice1.setText(String.valueOf(currentArray[0]));
+        Dice2.setText(String.valueOf(currentArray[1]));
+        Dice3.setText(String.valueOf(currentArray[2]));
+        Dice4.setText(String.valueOf(currentArray[3]));
+        Dice5.setText(String.valueOf(currentArray[4]));
+    }
+    public void SetPlayer1() {
+        List<Label> player1 = List.of(One, Two, Three, Four, Five, Six, ThreeOfAKind, FourofaKind, FullHouse, SmallStraight, LargeStraight, Chance, Yahtzee);
+        DiceLogic dice = new DiceLogic();
+        Integer[] counter = {dice.OneCounter(currentArray), dice.TwoCounter(currentArray), dice.ThreeCounter(currentArray), dice.FourCounter(currentArray), dice.FiveCounter(currentArray), dice.SixCounter(currentArray), dice.ThreeOfAKindCounter(currentArray), dice.FourOfAKindCounter(currentArray), dice.FullHouseCounter(currentArray), dice.checkForSmallStraight(currentArray), dice.checkForSmallStraight(currentArray), dice.ChanceCounter(currentArray), dice.YahtzeeCounter(currentArray)};
+        setText();
+        for (int i = 0; i < counter.length; i++) {
+            player1.get(i).setText(String.valueOf(counter[i]));
         }
-        return FourCounter;
     }
-
-    //create a function with the input from dicegenerator and returns the amount of 5's
-    public Integer FiveCounter(Integer[] Dice) {
-        Integer FiveCounter = 0;
-        for (int i = 0; i < DiceAmount; i++) {
-            if (Dice[i] == 5) {
-                FiveCounter++;
-            }
+    public void SetPlayer2() {
+        List<Label> player2 = List.of(One1, Two1, Three1, Four1, Five1, Six1, ThreeOfAKind1, FourofaKind1, FullHouse1, SmallStraight1, LargeStraight1, Chance1, Yahtzee1);
+        DiceLogic dice = new DiceLogic();
+        Integer[] counter = {dice.OneCounter(currentArray), dice.TwoCounter(currentArray), dice.ThreeCounter(currentArray), dice.FourCounter(currentArray), dice.FiveCounter(currentArray), dice.SixCounter(currentArray), dice.ThreeOfAKindCounter(currentArray), dice.FourOfAKindCounter(currentArray), dice.FullHouseCounter(currentArray), dice.checkForSmallStraight(currentArray), dice.checkForSmallStraight(currentArray), dice.ChanceCounter(currentArray), dice.YahtzeeCounter(currentArray)};
+        setText();for (int i = 0; i < counter.length; i++) {
+            player2.get(i).setText(String.valueOf(counter[i]));
         }
-        return FiveCounter;
-    }
-
-    //create a function with the input from dicegenerator and returns the amount of 6's
-    public Integer SixCounter(Integer[] Dice) {
-        Integer SixCounter = 0;
-        for (int i = 0; i < DiceAmount; i++) {
-            if (Dice[i] == 6) {
-                SixCounter++;
-            }
-        }
-        return SixCounter;
-    }
-
-    //create a function with the input from dicegenerator and returns the amount of 3 of a kind
-    public boolean ThreeOfAKindCounter(Integer[] Dice) {
-        for (int i = 0; i < Dice.length; i++) {
-            int count = 0;
-            for (int j = 0; j < Dice.length; j++) {
-                if (Dice[i] == Dice[j]) {
-                    count++;
-                }
-                if (count == 3) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    //create a function with the input from dicegenerator and returns the amount of 4 of a kind
-    public boolean FourOfAKindCounter(Integer[] Dice) {
-        for (int i = 0; i < Dice.length; i++) {
-            int count = 0;
-            for (int j = 0; j < Dice.length; j++) {
-                if (Dice[i] == Dice[j]) {
-                    count++;
-                }
-                if (count == 4) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    //create a function with the input from dicegenerator and returns the amount of full house
-    public boolean FullHouseCounter(Integer[] Dice) {
-        boolean FullHouseCounter = ThreeOfAKindCounter(Dice) && FourOfAKindCounter(Dice);
-        return FullHouseCounter;
-    }
-
-
-    //create a function with the input from dicegenerator and returns the amount of small straight
-    public boolean checkForSmallStraight(Integer[] Dice) {
-        for (int i = 0; i < Dice.length - 3; i++) {
-            int count = 0;
-            for (int j = i; j < Dice.length; j++) {
-                if (Dice[j] == Dice[i] + count) {
-                    count++;
-                }
-                if (count == 4) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    //create a function with the input from dicegenerator and returns the amount of large straight
-    public Boolean LargeStraightCounter(Integer[] Dice) {
-        for (int i = 0; i < Dice.length - 4; i++) {
-            int count = 0;
-            for (int j = i; j < Dice.length; j++) {
-                if (Dice[j] == Dice[i] + count) {
-                    count++;
-                }
-                if (count == 5) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
-    //create a function with the input from dicegenerator and returns the amount of chance
-    public Integer ChanceCounter(Integer[] Dice) {
-        int sum = 0;
-        for (int i = 0; i < Dice.length; i++) {
-            sum += Dice[i];
-        }
-        return sum;
-    }
-
-    //create a function with the input from dicegenerator and returns the amount of yahtzee
-    public boolean YahtzeeCounter(Integer[] Dice) {
-        return Arrays.stream(Dice).allMatch(val -> val == Dice[0]);
     }
 }
