f4a598f901c970efdb930584e610556e14baa1db
Elias Kupeczki <github@kunoxy.ch>
2023-02-14 16:10:43 +0100

    added sql connection for authentication

--- src/main/java/com/example/modul326/Login.java
+++ src/main/java/com/example/modul326/Login.java
@@ -2,9 +2,16 @@ package com.example.modul326;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.sql.*;
+import java.util.Objects;
 
 public class Login {
 
@@ -12,26 +19,43 @@ public class Login {
     private TextField usernameField;
     @FXML
     private PasswordField passwordField;
-    public boolean checkUsernamePassword(String username, String password) throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/yatzee", "root", "root");
-        String query = "SELECT * FROM authentication WHERE username = ? AND password = ?";
-        PreparedStatement pstmt = connection.prepareStatement(query);
-        pstmt.setString(1, username);
-        pstmt.setString(2, password);
-        ResultSet rs = pstmt.executeQuery();
-        if (rs.next()) {
-            return true;
-        }
-        return false;
-    }
+    private boolean checkUsernamePassword(String username, String password) throws SQLException {
+        //create connection to database mysql
+        try {
+            //for Future elias install it from maven
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/yatzee","root", "root");
+            String query = "SELECT * FROM authentication WHERE username = ? AND password = ?";
+            PreparedStatement pstmt = connect.prepareStatement(query);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                connect.close();
+                pstmt.close();
+                rs.close();
+                return true;
+            }
+        } catch (ClassNotFoundException e) {
 
-    // This is how the function is run when a button is clicked
+            e.printStackTrace();
+            return false;
+        }
+    return false;
+    }
     @FXML
-    private void handleButton(ActionEvent event) throws SQLException {
+    private void handleButton(ActionEvent event) throws SQLException, IOException {
         String username = usernameField.getText();
         String password = passwordField.getText();
         if (checkUsernamePassword(username, password)) {
             System.out.println("Login successful");
+            Stage stage = (Stage) usernameField.getScene().getWindow();
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } else {
+            System.out.println("Login failed");
         }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.modul326 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.j;
 
 
     opens com.example.modul326 to javafx.fxml;
